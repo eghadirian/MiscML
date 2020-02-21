3f7e377e78825fde2075fc6dd2551bc8d89e97e4
eghadirian <60559670+eghadirian@users.noreply.github.com>
2020-02-20 21:36:42 -0500

    typo

--- P2-CategorialInputData.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import sklearn as sk
-
-cat1 = pd.get_dummies()
-cat2 = sk.OneHotEncoder()
---
+++ P2-CategoricalInputData.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import sklearn as sk
+
+cat1 = pd.get_dummies()
+cat2 = sk.OneHotEncoder()
